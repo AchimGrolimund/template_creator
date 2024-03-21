447dee295c60c960277b3f54d6b6963702454918
Achim Grolimund <ac.grolimund@gmail.com>
2024-03-21 20:30:21 +0100

    update doc

--- internal/template/template.go
+++ internal/template/template.go
@@ -31,15 +31,30 @@ import (
 	"text/template"
 )
 
+/* The `embed` package directive is used to include the "imagestream.yaml" file located in the "templates" directory
+* into the binary at compile time. This file is then accessible as a string variable in the code.
+* This is useful for including static assets like configuration, HTML, CSS, images, or in this case, a YAML template.
+* The "imagestream.yaml" file is a template used for creating image streams in the application.
+ */
 //go:embed templates/imagestream.yaml
+
 var imagestreamTemplate string
 
+/* CreateTemplate is a function that creates a new template based on the provided namespace, type, and service.
+* It first checks if the namespace ends with "-prod" to set the environment.
+* Then, it creates a directory structure based on the namespace, type, environment, and stage.
+* It also creates a patches directory and a resources directory within the base directory.
+* After that, it parses the imagestream template and creates a new file in the resources directory.
+* Finally, it executes the template with the provided service and namespace and writes the output to the file.
+ */
 func CreateTemplate(namespace, _type, service string) error {
+	// Initialize a new logger
 	log, err := logger.NewLogger()
 	if err != nil {
 		panic(err)
 	}
-	// Check if namespace ends with -prod
+
+	// Check if namespace ends with -prod and set the environment accordingly
 	var env string
 	if strings.HasSuffix(namespace, "-prod") {
 		env = "prod"
@@ -47,13 +62,13 @@ func CreateTemplate(namespace, _type, service string) error {
 		env = "nonprod"
 	}
 
-	// Log with fields
+	// Log the set environment and namespace
 	log.Info("Setting environment",
 		zap.String("environment", env),
 		zap.String("namespace", namespace),
 	)
 
-	// Create directory structure
+	// Create directory structure based on the namespace, type, environment, and stage
 	lastIndex := strings.LastIndex(namespace, "-")
 	namespaceWithoutSuffix := ""
 	stage := ""
@@ -62,13 +77,14 @@ func CreateTemplate(namespace, _type, service string) error {
 		stage = namespace[lastIndex+1:]
 	}
 
-	// Log with fields
+	// Log the created directory structure
 	log.Info("Creating directory structure",
 		zap.String("namespaceWithoutSuffix", namespaceWithoutSuffix),
 		zap.String("stage", stage),
 	)
 	baseDir := fmt.Sprintf("./%s-%s-%s/%s", _type, namespaceWithoutSuffix, env, stage)
 
+	// Create a patches directory within the base directory
 	patchesDir := fmt.Sprintf("%s/patches", baseDir)
 	err = os.MkdirAll(patchesDir, os.ModePerm)
 	if err != nil {
@@ -77,6 +93,7 @@ func CreateTemplate(namespace, _type, service string) error {
 	}
 	log.Info("Created patches directory", zap.String("directory", patchesDir))
 
+	// Create a resources directory within the base directory
 	resourceesDir := fmt.Sprintf("%s/resources", baseDir)
 	err = os.MkdirAll(resourceesDir, os.ModePerm)
 	if err != nil {
@@ -85,7 +102,7 @@ func CreateTemplate(namespace, _type, service string) error {
 	}
 	log.Info("Created resources directory", zap.String("directory", resourceesDir))
 
-	// Render the imagestream template
+	// Parse the imagestream template
 	tmpl, err := template.New("imagestream").Parse(imagestreamTemplate)
 	if err != nil {
 		log.Error("Failed to parse template", zap.Error(err))
@@ -93,6 +110,7 @@ func CreateTemplate(namespace, _type, service string) error {
 	}
 	log.Info("Successfully parsed the imagestream template")
 
+	// Create a new file in the resources directory
 	imagestreamFile := fmt.Sprintf("%s/resources/imagestream_%s.yaml", baseDir, service)
 	f, err := os.Create(imagestreamFile)
 	if err != nil {
@@ -102,6 +120,7 @@ func CreateTemplate(namespace, _type, service string) error {
 	log.Info("Created imagestream file", zap.String("file", imagestreamFile))
 	defer f.Close()
 
+	// Execute the template with the provided service and namespace and write the output to the file
 	data := map[string]string{
 		"Name":      service,
 		"Namespace": namespace,
